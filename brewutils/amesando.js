a44aebea84f78366042b60f25ce3b32aecdc6f08
Vito Tai <vito@gmail.com>
2025-05-21 17:43:17 +0800

    add amasan

--- brewutils/amesando.js
+++ brewutils/amesando.js
@@ -1,34 +1,47 @@
+const bgImage2={
+ src:"nihonshu2.png",
+ width:446,
+ height:446,
+ xrange:[46,446],
+ yrange:[400,0],
+ shudos:[30,-30],
+ sandos:[0.2,2.6]
+};
 
-var bgImage={
+
+const bgImage={
+ src:"nihonshu.png",
  width:557,
  height:572,
- origin:[70,505],
- max:[544,29],
+ xrange:[70,544],
+ yrange:[550,29],
  shudos:[25,-30],
- sandos:[0.4,2.6],
- originMap:[25,0.4],
- maxMap:[-30,2.6]
+ sandos:[0.4,2.6]
 };
+
+
 const MarkSize=6;
 var AmeSan={
-init:function(cid,bgsrc){
+init:function(cid,bgInfo){
     // get div
     var canvas = document.getElementById(cid);
-    canvas.style.width = bgImage.width +'px';
-    canvas.style.height = bgImage.height + 'px';
+    canvas.style.width = bgInfo.width +'px';
+    canvas.style.height = bgInfo.height + 'px';
     const ctx = canvas.getContext("2d");
     const image = new Image();
-    image.src = bgsrc;
+    image.src = bgInfo.src;
     image.addEventListener("load", (e) => {
         ctx.drawImage(image, 0,0);
     });    
     this.ctx= ctx;
     this.bgImage=image;
+    this.bgInfo=bgInfo;
 },
 plotOn:function(shudo,sando){
+    const info = this.bgInfo;
+    var x =info.xrange[0] + (info.xrange[1] - info.xrange[0]) * (shudo - info.shudos[0])/ (info.shudos[1] - info.shudos[0]); 
+    var y =info.yrange[0] + (info.yrange[1] - info.yrange[0]) * (sando - info.sandos[0])/ (info.sandos[1] - info.sandos[0]); 
 
-    var x =bgImage.origin[0] + (bgImage.max[0] - bgImage.origin[0]) * (shudo - bgImage.shudos[0])/ (bgImage.shudos[1] - bgImage.shudos[0]); 
-    var y =bgImage.origin[1] + (bgImage.max[1] - bgImage.origin[1]) * (sando - bgImage.sandos[0])/ (bgImage.sandos[1] - bgImage.sandos[0]); 
     const ctx =this.ctx;
     // clear
     ctx.drawImage(this.bgImage, 0,0);
